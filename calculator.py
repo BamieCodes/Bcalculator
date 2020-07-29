6ec6ae879dccff6ad2548fd6626bc4ee6b75d25d
BamieCodes <67742847+BamieCodes@users.noreply.github.com>
2020-07-29 15:05:27 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to my calculator")
+first_number = int ( input("Enter the first number: "))
+second_number = int ( input("Enter the second number: "))
+sum_of_numbers = first_number + second_number
+print(sum_of_numbers)
+product_of_numbers =  first_number * second_number
+print(product_of_numbers)
